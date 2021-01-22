4b3d7327d10adee72a7eb5519dc38d006eb6b74d
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-01-22 17:57:45 +0100

    Make heading an empty struct

--- src/RelationalData.jl
+++ src/RelationalData.jl
@@ -6,20 +6,24 @@ export Heading, shapeto, Relation
 
 A `Heading` defines names and types for a relation.
 """
-struct Heading
-  names::Tuple{Vararg{Symbol}}
-  types::Tuple{Vararg{DataType}}
+struct Heading{names, T}
+  function Heading{names, T}() where {names, T<:Tuple}
+    sorted = (sort([names...])...,)
+    h = NamedTuple{names}(fieldtypes(T))
+    head = NamedTuple{sorted}(h)
+    new{sorted, Tuple{values(head)...}}()
+  end
 
   function Heading(h::NamedTuple{names, T} where {names, T<:Tuple{Vararg{DataType}}})
     sorted = (sort([keys(h)...])...,)
     head = NamedTuple{sorted}(h)
-    new(sorted, values(head))
+    new{sorted, Tuple{values(head)...}}()
   end
 
   function Heading(h::NamedTuple)
     sorted = (sort([keys(h)...])...,)
     head = NamedTuple{sorted}(h)
-    new(sorted, (typeof(head).types...,))
+    new{sorted, Tuple{typeof(head).types...}}()
   end
 end
 
@@ -30,10 +34,12 @@ Returns the named tuple converted to conform to the heading with attributes sort
 and types converted as appropriate (only safe conversions). Throws if conversion is impossible.
 """
 function shapeto(nt::NamedTuple, h::Heading)
-  length(nt) == length(h.names) || throw(DomainError(nt, "Does not match $h"))
+  head = typeof(h)
+  names = head.parameters[1]
+  length(nt) == length(names) || throw(DomainError(nt, "Does not match $h"))
   # For some reason we can't get the order right for the types here
-  ont = NamedTuple{h.names}(nt)
-  convert(NamedTuple{h.names, Tuple{h.types...}}, ont)
+  ont = NamedTuple{names}(nt)
+  convert(NamedTuple{names, head.parameters[2]}, ont)
 end
 
 struct Relation
